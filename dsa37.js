93924d9865f00220986dcf3c4f1e997da8dc06dd
Tushar <tushargupta7584@gmail.com>
2022-01-01 16:18:30 +0530

    refactor code

--- dsa37.js
+++ dsa37.js
@@ -6,6 +6,7 @@
  */
 const checkIfArithmetic = (arr) => {
   arr.sort((a, b) => a - b);
+
   const diff = arr[1] - arr[0];
   for (let i = 0; i < arr.length - 1; i++) {
     if (arr[i + 1] - arr[i] !== diff) {
