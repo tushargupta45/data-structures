69eeba6a9d87c7b1e4c2c6cf1b45aa72b09dd958
Tushar <tushargupta7584@gmail.com>
2022-09-26 16:23:59 +0530

    refactor code

--- dsa44.js
+++ dsa44.js
@@ -1,3 +1,10 @@
+//https://leetcode.com/problems/binary-search/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
 var search = function (nums, target) {
   let l = 0;
   let r = nums.length - 1;
