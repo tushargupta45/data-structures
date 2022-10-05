674947db4f75f2e1a6e23a27cb193f70ad954913
Tushar <tushargupta7584@gmail.com>
2022-10-05 16:25:33 +0530

    refactor code

---
+++ dsa45.js
@@ -0,0 +1,22 @@
+var solution = function (isBadVersion) {
+  return function (n) {
+    if (n === 1 || isBadVersion(1)) {
+      return 1;
+    }
+    let left = 1;
+    let right = n;
+    while (left < right) {
+      let mid = left + Math.floor((right - left) / 2);
+      if (isBadVersion(mid) && isBadVersion(mid + 1)) {
+        right = mid - 1;
+      } else if (!isBadVersion(mid) && !isBadVersion(mid + 1)) {
+        left = mid + 1;
+      } else {
+        return mid + 1;
+      }
+    }
+    if (right === left) {
+      return left + 1;
+    } else return left;
+  };
+};
