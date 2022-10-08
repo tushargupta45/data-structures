f151fd38a329377d35ee7b5776d762a2ae9798d5
Tushar <tushargupta7584@gmail.com>
2022-10-08 16:25:53 +0530

    refactor code

--- dsa45.js
+++ dsa45.js
@@ -1,4 +1,14 @@
 /**
+ * Definition for isBadVersion()
+ *
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
  * @param {function} isBadVersion()
  * @return {function}
  */
