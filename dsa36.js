9dbca8296362a399c27f7b68d244bcc1e5fb6e7a
Tushar <tushargupta7584@gmail.com>
2022-10-23 16:15:57 +0530

    refactor code

--- dsa36.js
+++ dsa36.js
@@ -7,11 +7,9 @@
 const diagonalSort = (matrix) => {
   const m = matrix.length;
   const n = matrix[0].length;
-
   if (m === 1) {
     return matrix;
   }
-
   for (let i = 0; i < m + n - 1; i++) {
     //add code here
   }
