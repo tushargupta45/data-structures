680c643821ca9c4af497e85357fd4d3ad0e9f59e
Tushar <tushargupta7584@gmail.com>
2022-11-14 16:21:02 +0530

    refactor code

--- dsa38.js
+++ dsa38.js
@@ -8,6 +8,7 @@ const partitionArray = (nums, k) => {
   nums.forEach((num, i) => dataIndexArr.push([num, i]));
   dataIndexArr.sort((a, b) => a[0] - b[0]);
   let subSeqIndex = 0;
+
   let result = [];
   for (let i = 1; i < dataIndexArr.length; i++) {
     if (dataIndexArr[i][0] - dataIndexArr[i - 1][0] <= k) {
