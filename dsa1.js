56aa2c88fa38862426bb2523bed884049fe91102
Tushar <tushargupta7584@gmail.com>
2022-12-21 12:05:40 +0530

    refactor code

--- dsa1.js
+++ dsa1.js
@@ -1,4 +1,6 @@
-var buildArray = function (nums) {
+//https://leetcode.com/problems/build-array-from-permutation/solutions/
+
+const buildArray = function (nums) {
   if (nums.length < 1 || nums.length > 1000) {
     return -1;
   }
@@ -9,6 +11,5 @@ var buildArray = function (nums) {
   });
   return nums.map((el) => nums[el]);
 };
-
 const a = [0, 2, 1, 5, 3, 4];
 console.log(buildArray(a));
