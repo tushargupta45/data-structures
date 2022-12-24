72ebfadd33eab902957af67bfaafbb63b23ca498
Tushar <tushargupta7584@gmail.com>
2022-12-24 12:08:07 +0530

    add dsa file

---
+++ dsa2.js
@@ -0,0 +1,15 @@
+var kidsWithCandies = function (candies, extraCandies) {
+  const maxCandies = Math.max(...candies);
+  const output = [];
+  candies.forEach((candy, index) => {
+    if (candy + extraCandies >= maxCandies) {
+      output[index] = true;
+    } else {
+      output[index] = false;
+    }
+  });
+  return output;
+};
+
+const c = [2, 3, 5, 1, 3];
+console.log(kidsWithCandies(c, 3));
