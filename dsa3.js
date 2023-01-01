5220a5652cce5eca6e9a17460ea32de9eec55e55
Tushar <tushargupta7584@gmail.com>
2023-01-01 12:16:00 +0530

    refactor code

--- dsa3.js
+++ dsa3.js
@@ -2,13 +2,12 @@
 
 const sumOddLengthSubarrays = (arr) => {
   let result = 0;
-
   for (let i = 0; i < arr.length; ++i) {
     result += parseInt(((i + 1) * (arr.length - i) + 1) / 2) * arr[i];
   }
 
   return result;
 };
-const a = [10, 11, 12];
 
+const a = [10, 11, 12];
 console.log(sumOddLengthSubarrays(a));
