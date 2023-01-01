8141898b66add0d858f5e7ac33f55c0827b92f8f
Tushar <tushargupta7584@gmail.com>
2023-01-01 12:14:55 +0530

    add dsa file

---
+++ dsa3.js
@@ -0,0 +1,11 @@
+var sumOddLengthSubarrays = function (arr) {
+  let result = 0;
+
+  for (let i = 0; i < arr.length; ++i) {
+    result += parseInt(((i + 1) * (arr.length - i) + 1) / 2) * arr[i];
+  }
+
+  return result;
+};
+const a = [10, 11, 12];
+console.log(sumOddLengthSubarrays(a));
