ba177add75c73732bc7dfac18e3ea2942a9a99ea
Tushar <tushargupta7584@gmail.com>
2023-01-11 12:22:31 +0530

    added dsa file

---
+++ dsa5.js
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/find-first-palindromic-string-in-the-array/
+
+var firstPalindrome = function (words) {
+  if (words.length > 100 || words.length < 1) {
+    return "";
+  }
+  const palindrome = words.find((word) => {
+    if (word.length > 100 || word.length < 1) {
+      return "";
+    }
+    return word === word.split("").reverse().join("");
+  });
+  if (palindrome) {
+    return palindrome;
+  }
+  return "";
+};
+
+const a = ["abc", "car", "racecar", "cool"];
+console.log(firstPalindrome(a));
