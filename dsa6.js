9c9f65fadbba7148414738797659ac5d2f5c0975
Tushar <tushargupta7584@gmail.com>
2023-01-20 12:25:30 +0530

    add dsa file

---
+++ dsa6.js
@@ -0,0 +1,15 @@
+var prefixCount = function (words, pref) {
+  if (pref.length > 100 || words.length > 100 || words.length < 1) {
+    return 0;
+  }
+  let count = 0;
+  words.forEach((word) => {
+    if (word.startsWith(pref)) {
+      count++;
+    }
+  });
+  return count;
+};
+
+const a = ["pay", "attention", "practice", "attend"];
+console.log(prefixCount(a, "at"));
