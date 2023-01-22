269c8d9edaa88bd0dc1a40837a0926073f9da849
Tushar <tushargupta7584@gmail.com>
2023-01-22 12:26:54 +0530

    refactor code

--- dsa6.js
+++ dsa6.js
@@ -4,18 +4,14 @@ const countPrefix = (words, prefix) => {
   if (prefix.length > 100 || words.length > 100 || words.length < 1) {
     return 0;
   }
-
   let count = 0;
-
   words.forEach((word) => {
     if (word.startsWith(prefix)) {
       count++;
     }
   });
-
   return count;
 };
 
 const a = ["pay", "attention", "practice", "attend"];
-
 console.log(countPrefix(a, "at"));
