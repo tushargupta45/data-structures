9725a53b3dd33a8024bd8ffd78a054f0ef12b3d1
Tushar <tushargupta7584@gmail.com>
2023-01-24 12:27:19 +0530

    add new dsa file

---
+++ dsa7.js
@@ -0,0 +1,15 @@
+var numberOfPairs = function (nums) {
+  let pairCount = 0;
+  let i = 0;
+  nums = nums.sort((a, b) => a - b);
+  while (i < nums.length - 1) {
+    if (nums[i] === nums[i + 1]) {
+      pairCount++;
+      i++;
+    }
+    i++;
+  }
+  return [pairCount, nums.length - 2 * pairCount];
+};
+
+console.log(numberOfPairs([1, 3, 2, 1, 3, 2, 2]));
