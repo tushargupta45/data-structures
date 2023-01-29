413355e1955b6f9edeb53159b91b98f2d5e89118
Tushar <tushargupta7584@gmail.com>
2023-01-29 12:56:17 +0530

    add new dsa file

---
+++ dsa8.js
@@ -0,0 +1,19 @@
+var heightChecker = function (heights) {
+  if (heights.length > 100 || heights.length < 1) {
+    return 0;
+  }
+  const initialHeights = [...heights];
+  let unmatchedIndices = 0;
+  heights.sort((a, b) => a - b);
+  initialHeights.forEach((el, i) => {
+    if (el > 100 || el < 1) {
+      unmatchedIndices = 0;
+      return;
+    } else if (el !== heights[i]) {
+      unmatchedIndices++;
+    }
+  });
+  return unmatchedIndices;
+};
+
+console.log(heightChecker([1, 1, 4, 2, 1, 3]));
