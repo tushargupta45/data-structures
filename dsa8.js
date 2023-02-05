c4d1a52621587b103d81b005ff61eb6994dabb38
Tushar <tushargupta7584@gmail.com>
2023-02-05 12:58:35 +0530

    refactor code

--- dsa8.js
+++ dsa8.js
@@ -4,10 +4,12 @@ const checkHeight = (heightsArr) => {
   if (heightsArr.length > 100 || heightsArr.length < 1) {
     return 0;
   }
+
   const initialHeights = [...heightsArr];
   let unmatchedIndices = 0;
 
   heightsArr.sort((a, b) => a - b);
+
   initialHeights.forEach((el, i) => {
     if (el > 100 || el < 1) {
       unmatchedIndices = 0;
@@ -18,4 +20,5 @@ const checkHeight = (heightsArr) => {
   });
   return unmatchedIndices;
 };
+
 console.log(checkHeight([1, 1, 4, 2, 1, 3]));
