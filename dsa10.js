d8b484a7152fcdd92aac1f31718e3422cef9352e
Tushar <tushargupta7584@gmail.com>
2023-02-15 13:00:59 +0530

    add new file

---
+++ dsa10.js
@@ -0,0 +1,14 @@
+var maximumValue = function (strs) {
+  let high = 0;
+  strs.forEach((el) => {
+    if (isNaN(Number(el))) {
+      high = Math.max(high, el.length);
+    } else {
+      high = Math.max(high, parseInt(el));
+    }
+  });
+  return high;
+};
+
+const strs = ["alic3", "bob", "3", "4", "00000"];
+console.log(maximumValue(strs));
