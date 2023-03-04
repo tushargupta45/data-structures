6348c284ecabdb313febf157094109e99606c699
Tushar <tushargupta7584@gmail.com>
2023-03-04 13:04:26 +0530

    refactor code

--- dsa11.js
+++ dsa11.js
@@ -2,14 +2,15 @@
 
 const allCellsDistOrder = (rows, cols, rCenter, cCenter) => {
   const matrix = [];
-
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
       const distance = Math.abs(rCenter - i) + Math.abs(cCenter - j);
+
       matrix.push([i, j, distance]);
     }
   }
   matrix.sort((a, b) => a[2] - b[2]);
+
   return matrix.map((row) => [row[0], row[1]]);
 };
 
