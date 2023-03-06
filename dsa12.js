e6addccba85ff01fb4306d96054f9c664e6406ab
Tushar <tushargupta7584@gmail.com>
2023-03-06 13:05:02 +0530

    add new dsa file

---
+++ dsa12.js
@@ -0,0 +1,20 @@
+var canMakeArithmeticProgression = function (arr) {
+  const infinite = 10 ** 6;
+  if (arr.length > 1000 || arr.length <= 1) {
+    return false;
+  }
+  arr.sort((a, b) => a - b);
+  const gap = arr[1] - arr[0];
+
+  for (let i = 1; i < arr.length - 1; i++) {
+    if (arr[i] > infinite || arr[i] < -infinite) {
+      return false;
+    }
+    if (arr[i + 1] - arr[i] !== gap) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(canMakeArithmeticProgression([3, 5, 1]));
