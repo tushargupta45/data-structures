a031324ba0a7c311659a97b09c5bed6e3b700df7
Tushar <tushargupta7584@gmail.com>
2023-03-11 13:05:51 +0530

    refactor code

--- dsa12.js
+++ dsa12.js
@@ -5,9 +5,9 @@ const canMakeArithmeticProgression = (arr) => {
   if (arr.length > 1000 || arr.length <= 1) {
     return false;
   }
+
   arr.sort((a, b) => a - b);
   const gap = arr[1] - arr[0];
-
   for (let i = 1; i < arr.length - 1; i++) {
     if (arr[i] > infinite || arr[i] < -infinite) {
       return false;
