9181b55dadf192ccd1b4aad4c773fa9f8c5fe02c
Tushar <tushargupta7584@gmail.com>
2023-03-25 13:08:07 +0530

    refactor code

--- dsa13.js
+++ dsa13.js
@@ -1,6 +1,6 @@
 //https://leetcode.com/problems/two-furthest-houses-with-different-colors/
 
-const maxDistance = function (colors) {
+const maxDistance = (colors) => {
   let length = colors.length - 1;
   while (length > 0) {
     for (let i = 0; i < colors.length - length; i++) {
