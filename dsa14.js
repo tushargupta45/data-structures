a693e664ff28f97bd7aedc0a6de3f3a5ce9deffa
Tushar <tushargupta7584@gmail.com>
2023-03-28 13:09:16 +0530

    add file

---
+++ dsa14.js
@@ -0,0 +1,19 @@
+var lastStoneWeight = function (stones) {
+  stones.sort((a, b) => b - a);
+  while (stones.length > 1) {
+    const stone1 = stones.shift();
+    const stone2 = stones.shift();
+    if (stone1 - stone2) {
+      stones.push(Math.abs(stone1 - stone2));
+      stones.sort((a, b) => b - a);
+    }
+  }
+  if (stones[0]) {
+    return stones[0];
+  } else {
+    return 0;
+  }
+};
+
+const arr = [2, 7, 4, 1, 8, 1];
+console.log(lastStoneWeight(arr));
