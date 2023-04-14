f16f63d070b025eb4e0d19946826da8d2db4c75c
Tushar <tushargupta7584@gmail.com>
2023-04-14 13:14:06 +0530

    refactor code

--- dsa15.js
+++ dsa15.js
@@ -1,17 +1,17 @@
 //https://leetcode.com/problems/find-the-original-array-of-prefix-xor/
 
-const findArray = (pref) => {
-  if (pref.length < 1 || pref.length > 10 ** 5) {
+const findArray = (prefArr) => {
+  if (prefArr.length < 1 || prefArr.length > 10 ** 5) {
     return [];
   }
-  const result = [pref[0]];
-  let xor = pref[0];
-  for (let i = 1; i < pref.length; i++) {
-    if (pref[i] < 0 || pref[i] > 10 ** 6) {
+  const result = [prefArr[0]];
+  let xor = prefArr[0];
+  for (let i = 1; i < prefArr.length; i++) {
+    if (prefArr[i] < 0 || prefArr[i] > 10 ** 6) {
       return [];
     }
-    result.push(xor ^ pref[i]);
-    xor = pref[i];
+    result.push(xor ^ prefArr[i]);
+    xor = prefArr[i];
   }
   return result;
 };
