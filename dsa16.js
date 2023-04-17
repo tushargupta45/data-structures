25add337ac152e45b7247792cca91f685ff9e88e
Tushar <tushargupta7584@gmail.com>
2023-04-17 13:14:46 +0530

    add file

---
+++ dsa16.js
@@ -0,0 +1,32 @@
+var findingUsersActiveMinutes = function (logs, k) {
+  logs.sort((a, b) => a[0] - b[0]);
+
+  const id = new Map();
+  for (let i = 0; i < logs.length; i++) {
+    if (!id.has(logs[i][0])) {
+      id.set(logs[i][0], []);
+    }
+    id.get(logs[i][0]).push(logs[i][1]);
+  }
+
+  const result = [];
+  for (let i = 0; i < k; i++) {
+    result.push(0);
+  }
+
+  id.forEach((val, key) => {
+    const set = new Set(val);
+    result[set.size - 1]++;
+  });
+
+  return result;
+};
+
+const l = [
+  [0, 5],
+  [1, 2],
+  [0, 2],
+  [0, 5],
+  [1, 3],
+];
+console.log(findingUsersActiveMinutes(l, 5));
