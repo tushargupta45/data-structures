3dd04d4a062d329566c20d57c8e6bad4ad619098
Tushar <tushargupta7584@gmail.com>
2023-04-25 13:17:40 +0530

    add file

---
+++ dsa17.js
@@ -0,0 +1,10 @@
+//https://leetcode.com/problems/design-a-stack-with-increment-operation/
+
+//STACK IMPLEMENTATON
+
+class CustomStack {
+  constructor(maxSize) {
+    this.stack = [];
+    this.maxSize = maxSize;
+  }
+}
