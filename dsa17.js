c11c7f38a1fd9a1a928397569ea47deaab507274
Tushar <tushargupta7584@gmail.com>
2023-04-25 13:18:17 +0530

    refactor code

--- dsa17.js
+++ dsa17.js
@@ -32,3 +32,16 @@ CustomStack.prototype.increment = function (k, val) {
     this.stack[i] += val;
   }
 };
+
+/**
+ * Your CustomStack object will be instantiated and called as such:
+ * var obj = new CustomStack(maxSize)
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * obj.increment(k,val)
+ */
+
+const obj = new CustomStack(3);
+obj.push(1);
+obj.push(2);
+console.log(obj);
