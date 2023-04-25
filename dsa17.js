fffe40ce4fb0d52bf99435141c5ee882d0e76735
Tushar <tushargupta7584@gmail.com>
2023-04-25 13:18:00 +0530

    refactor code

--- dsa17.js
+++ dsa17.js
@@ -8,3 +8,27 @@ class CustomStack {
     this.maxSize = maxSize;
   }
 }
+
+//x is number
+CustomStack.prototype.push = function (x) {
+  if (this.stack.length < this.maxSize) {
+    this.stack.push(x);
+  }
+};
+
+//return number
+CustomStack.prototype.pop = function () {
+  if (this.stack.length) {
+    return this.stack.pop();
+  } else {
+    return -1;
+  }
+};
+
+//both input nums
+CustomStack.prototype.increment = function (k, val) {
+  const length = Math.min(k, this.stack.length);
+  for (let i = 0; i < length; i++) {
+    this.stack[i] += val;
+  }
+};
