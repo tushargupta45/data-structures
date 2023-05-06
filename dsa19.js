5951861e0a0f98f30084895ad8f1f3c093e6d224
Tushar <tushargupta7584@gmail.com>
2023-05-06 13:22:49 +0530

    refactor code

---
+++ dsa19.js
@@ -0,0 +1,38 @@
+var displayTable = function (orders) {
+  const result = [["Table"]];
+  const a = [];
+  const foodItems = orders.map((order) => order[2]);
+  foodItems.sort();
+  const uniqueFoodItems = new Set(foodItems);
+  uniqueFoodItems.forEach((val) => {
+    result[0].push(val);
+    a.push("0");
+  });
+  const tables = orders.map((order) => order[1]);
+  tables.sort((a, b) => a - b);
+  const uniqueTables = new Set(tables);
+  uniqueTables.forEach((table) => {
+    result.push([table, ...a]);
+    for (let i = 1; i < result[0].length; i++) {
+      for (let j = 0; j < orders.length; j++) {
+        if (orders[j][1] === table && orders[j][2] === result[0][i]) {
+          result[result.length - 1][i] = (
+            parseInt(result[result.length - 1][i]) + 1
+          ).toString();
+        }
+      }
+    }
+  });
+  return result;
+};
+
+const o = [
+  ["David", "3", "Ceviche"],
+  ["Corina", "10", "Beef Burrito"],
+  ["David", "3", "Fried Chicken"],
+  ["Carla", "5", "Water"],
+  ["Carla", "5", "Ceviche"],
+  ["Rous", "3", "Ceviche"],
+];
+
+console.log(displayTable(o));
