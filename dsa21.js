a424cea68a5de2c10df20219ab4798742939a5c6
Tushar <tushargupta7584@gmail.com>
2023-05-16 13:26:43 +0530

    refactor code

---
+++ dsa21.js
@@ -0,0 +1,20 @@
+var rotate = function (matrix) {
+  const l = matrix.length;
+  matrix.reverse();
+  let swap;
+  for (let i = 0; i < l; i++) {
+    for (let j = i; j < l; j++) {
+      swap = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = swap;
+    }
+  }
+};
+
+const m = [
+  [5, 1, 9, 11],
+  [2, 4, 8, 10],
+  [13, 3, 6, 7],
+  [15, 14, 12, 16],
+];
+rotate(m);
