fb17f58110d175150289cdccf162581d43baa380
Tushar <tushargupta7584@gmail.com>
2023-05-21 13:29:54 +0530

    refactor code

--- dsa23.js
+++ dsa23.js
@@ -16,7 +16,7 @@ const likeTimeCoefficient = (arr) => {
  * @param {number[]} satisfaction
  * @return {number}
  */
-const maxSatisfaction = function (satisfaction) {
+const maxSatisfaction = (satisfaction) => {
   if (satisfaction.length > 500 || satisfaction.length < 1) {
     return 0;
   }
