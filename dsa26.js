4a1d274eb0fe17553d6d9df3fca6711ba7cacf94
Tushar <tushargupta7584@gmail.com>
2023-06-03 13:31:57 +0530

    refactor code

---
+++ dsa26.js
@@ -0,0 +1,16 @@
+var minimumSum = function (num) {
+  if (1000 > num || num > 9999) {
+    return 0;
+  }
+  const arr = [];
+  arr.push(Math.floor(num / 1000));
+  arr.push(Math.floor(num / 100) % 10);
+  arr.push(Math.floor(num / 10) % 10);
+  arr.push(num % 10);
+
+  arr.sort((a, b) => a - b);
+
+  return arr[0] * 10 + arr[1] * 10 + arr[2] + arr[3];
+};
+
+console.log(minimumSum(2932));
