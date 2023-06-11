4958ed5c4651e119b164237e89cfaab061d4602e
Tushar <tushargupta7584@gmail.com>
2023-06-11 13:34:13 +0530

    refactor code

--- dsa27.js
+++ dsa27.js
@@ -5,7 +5,7 @@
  * @param {number[][]} items2
  * @return {number[][]}
  */
-var mergeSimilarItems = function (items1, items2) {
+const mergeSimilarItems = function (items1, items2) {
   const result = [];
   const set = new Set();
   items1.forEach((i) => set.add(i[0]));
