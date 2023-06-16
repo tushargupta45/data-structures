892d855cc6a149ca83eda388d8a222e1b9a031d6
Tushar <tushargupta7584@gmail.com>
2023-06-16 13:35:17 +0530

    refactor code

---
+++ dsa28.js
@@ -0,0 +1,14 @@
+var smallerNumbersThanCurrent = function (nums) {
+  if (nums.length < 2 || nums.length > 500) {
+    return [];
+  }
+  const initialNums = [...nums];
+  nums.sort((a, b) => a - b);
+  return initialNums.map((num) => {
+    if (num >= 0 && num <= 100) {
+      return nums.findIndex((n) => n === num);
+    }
+  });
+};
+
+console.log(smallerNumbersThanCurrent([8, 1, 2, 2, 3]));
