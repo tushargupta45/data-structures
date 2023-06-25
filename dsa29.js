89df184929f62439ad176faab5bf80bb9cd2bc3b
Tushar <tushargupta7584@gmail.com>
2023-06-25 13:37:29 +0530

    refactor code

--- dsa29.js
+++ dsa29.js
@@ -10,7 +10,9 @@ const maximumUnits = (boxTypes, truckSize) => {
   let units = 0;
   let currentIndex = 0;
   let iterations = 0;
+
   boxTypes.forEach((box, i) => (iterations += box[0]));
+
   for (let i = 0; i < Math.min(iterations, truckSize); i++) {
     if (boxTypes[currentIndex][0] > 0) {
       boxTypes[currentIndex][0]--;
