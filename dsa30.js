93463e0ea69a5b54f6d3b793d4b14e1da1d95a49
Tushar <tushargupta7584@gmail.com>
2023-06-30 13:39:05 +0530

    refactor code

--- dsa30.js
+++ dsa30.js
@@ -1,3 +1,8 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
 var intersection = function (nums1, nums2) {
   nums1.sort((a, b) => a - b);
   nums2.sort((a, b) => a - b);
