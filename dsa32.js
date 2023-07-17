de1a087667c632f859f2ea14ac676a38d7c28444
Tushar <tushargupta7584@gmail.com>
2023-07-17 13:42:32 +0530

    refactor code

--- dsa32.js
+++ dsa32.js
@@ -1,3 +1,9 @@
+//https://leetcode.com/problems/missing-number
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 var missingNumber = function (nums) {
   return (
     (nums.length * (nums.length + 1)) / 2 - nums.reduce((a, b) => a + b, 0)
